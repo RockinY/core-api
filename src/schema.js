0eb886fe5ee498585c4b15e8b0eb23e05530301f
Bran Liang <lby89757@hotmail.com>
2018-05-08 23:45:34 +0800

    Add basic schema

---
+++ src/schema.js
@@ -0,0 +1,67 @@
+// @flow
+import UserError from './utils/userError'
+import {
+  makeExecutableSchema,
+  addSchemaLevelResolveFunction
+} from 'graphql-tools'
+import { merge } from 'lodash'
+
+const debug = require('debug')('api:resolvers')
+const logExecutions = require('graphql-log')({
+  logger: debug
+})
+
+// Types
+const User = require('./types/User')
+
+// Queries
+const userQueries = require('./queries/user')
+
+const Root = `
+  type Query {
+    dummy: String
+  }
+
+  type Mutation {
+    dummy: String
+  }
+
+  type Subscription {
+    dummy: String
+  }
+
+  schema {
+    query: Query
+    mutation: Mutation
+    subscription: Subscription
+  }
+`
+
+const resolvers = merge(
+  {},
+  // Queries
+  userQueries
+)
+
+if (process.env.NODE_ENV === 'development' && debug.enabled) {
+  logExecutions(resolvers)
+}
+
+const schema = makeExecutableSchema({
+  typeDefs: [
+    Root,
+    User
+  ],
+  resolvers
+})
+
+if (process.env.REACT_APP_MAINTENANCE_MODE === 'enabled') {
+  console.error('\n\n⚠️ ----MAINTENANCE MODE ENABLED----⚠️\n\n')
+  addSchemaLevelResolveFunction(schema, () => {
+    throw new UserError(
+      "We're currently undergoing planned maintenance!"
+    )
+  })
+}
+
+module.exports = schema
