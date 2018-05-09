60518013218e1b4a0e3d1ed96f4f12530faf3e09
Bran Liang <lby89757@hotmail.com>
2018-05-09 15:18:16 +0800

    Add basic types

---
+++ src/types.js
@@ -0,0 +1,39 @@
+// @flow
+/* ----------- Database ----------- */
+export type DBUser = {
+  id: string,
+  email?: string,
+  createdAt: Date,
+  name: string,
+  coverPhoto: string,
+  profilePhoto: string,
+  providerId?: ?string,
+  githubProviderId?: ?string,
+  githubUsername?: ?string,
+  username: ?string,
+  timezone?: ?number,
+  isOnline?: boolean,
+  lastSeen?: ?Date,
+  description?: ?string,
+  website?: ?string,
+  modifiedAt: ?Date
+}
+
+/* ----------- Loader ----------- */
+export type Loader = {
+  load: (key: string | Array<string>) => Promise<any>,
+  loadMany: (keys: Array<string>) => Promise<any>,
+  clear: (key: string | Array<string>) => void
+}
+
+export type DataLoaderOptions = {
+  cache?: boolean
+}
+
+/* ----------- GraphQL ----------- */
+export type GraphQLContext = {
+  user: DBUser,
+  loaders: {
+    [key: string]: Loader
+  }
+}
