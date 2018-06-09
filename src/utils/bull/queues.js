902a814a608ff88fc06799d8245bd2b13a172434
Bran Liang <lby89757@hotmail.com>
2018-06-09 16:53:08 +0800

    Add bull queues

---
+++ src/utils/bull/queues.js
@@ -0,0 +1,25 @@
+// @flow
+import createQueue from './create-queue'
+import type { Queues } from './types'
+import EventEmitter from 'events'
+
+import {
+  CHANNEL_NOTIFICATION
+} from '../athenaConstants'
+
+exports.QUEUE_NAMES = {
+  // notifications
+  sendChannelNotificationQueue: CHANNEL_NOTIFICATION,
+}
+
+EventEmitter.defaultMaxListeners = Object.keys(exports.QUEUE_NAMES).length + EventEmitter.defaultMaxListeners
+
+const queues: Queues = Object.keys(exports.QUEUE_NAMES).reduce(
+  (queues, name) => {
+    queues[name] = createQueue(exports.QUEUE_NAMES[name])
+    return queues
+  },
+  {}
+)
+
+module.exports = queues
