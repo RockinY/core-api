e8aa8ee43ba8a1143b3048227cb47294abf9c3be
Bran Liang <lby89757@hotmail.com>
2018-06-12 20:17:21 +0800

    Add model curated content

---
+++ src/models/curatedContent.js
@@ -0,0 +1,14 @@
+//@flow
+import db from '../db.js'
+import type { DBCommunity } from '../flowTypes'
+import { getCommunitiesBySlug } from './community'
+
+// prettier-ignore
+export const getCuratedCommunities = (type: string): Promise<Array<DBCommunity>> => {
+  return db
+    .table('curatedContent')
+    .filter({ type })
+    .run()
+    .then(results => (results && results.length > 0 ? results[0] : null))
+    .then(result => result && getCommunitiesBySlug(result.data));
+};
