557b7b703c4ee98df5a6f3aaa6244c106717e001
Bran Liang <lby89757@hotmail.com>
2018-06-13 19:16:49 +0800

    Add model user

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,8 @@
 // @flow
 import db from '../db.js'
 import { createNewUsersSettings } from './usersSettings.js'
+import { uploadImage } from '../utils/oss'
+import type { PaginationOptions } from '../utils/paginateArrays'
 import type { DBUser, FileUpload } from '../flowtypes'
 
 type GetUserInput = {
@@ -172,6 +174,33 @@ const getUsersByUsername = (
     .run()
 }
 
+const getEverything = (userId: string, options: PaginationOptions): Promise<Array<any>> => {
+  const { first, after } = options
+  return db
+    .table('usersChannels')
+    .getAll(userId, { index: 'userId' })
+    .filter(userChannel => userChannel('isMember').eq(true))
+    .map(userChannel => userChannel('channelId'))
+    .run()
+    .then(
+      userChannels =>
+        userChannels &&
+        userChannels.length > 0 &&
+        db
+          .table('threads')
+          .orderBy({ index: db.desc('lastActive') })
+          .filter(thread =>
+            db
+              .expr(userChannels)
+              .contains(thread('channelId'))
+              .and(db.not(thread.hasFields('deletedAt')))
+          )
+          .skip(after || 0)
+          .limit(first)
+          .run()
+    )
+}
+
 export type EditUserInput = {
   input: {
     file?: FileUpload,
@@ -184,10 +213,17 @@ export type EditUserInput = {
   }
 }
 
-const editUser = (input: EditUserInput, userId: string): Promise<DBUser> => {
+const editUser = (args: EditUserInput, userId: string): Promise<DBUser> => {
   const {
-    input: { name, description, website, file, coverFile, username, timezone }
-  } = input
+    name,
+    description,
+    website,
+    file,
+    coverFile,
+    username,
+    timezone
+  } = args.input
+
   return db
     .table('users')
     .get(userId)
@@ -204,7 +240,117 @@ const editUser = (input: EditUserInput, userId: string): Promise<DBUser> => {
     })
     .then(user => {
       if (file || coverFile) {
-        // TODO: Upload images
+        if (file && !coverFile) {
+          return uploadImage(file, 'users', user.id)
+            .then(profilePhoto => {
+              // update the user with the profilePhoto
+              return (
+                db
+                  .table('users')
+                  .get(user.id)
+                  .update(
+                    {
+                      ...user,
+                      profilePhoto
+                    },
+                    { returnChanges: 'always' }
+                  )
+                  .run()
+                  // return the resulting user with the profilePhoto set
+                  .then(result => {
+                    // if an update happened
+                    if (result.replaced === 1) {
+                      return result.changes[0].new_val
+                    }
+
+                    // an update was triggered from the client, but no data was changed
+                    if (result.unchanged === 1) {
+                      return result.changes[0].old_val
+                    }
+                  })
+              )
+            })
+            .catch(err => {
+              console.error(err)
+            })
+        } else if (!file && coverFile) {
+          return uploadImage(coverFile, 'users', user.id)
+            .then(coverPhoto => {
+              // update the user with the profilePhoto
+              return (
+                db
+                  .table('users')
+                  .get(user.id)
+                  .update(
+                    {
+                      ...user,
+                      coverPhoto
+                    },
+                    { returnChanges: 'always' }
+                  )
+                  .run()
+                  // return the resulting user with the profilePhoto set
+                  .then(result => {
+                    // if an update happened
+                    if (result.replaced === 1) {
+                      return result.changes[0].new_val
+                    }
+
+                    // an update was triggered from the client, but no data was changed
+                    if (result.unchanged === 1) {
+                      return result.changes[0].old_val
+                    }
+                  })
+              )
+            })
+            .catch(err => {
+              console.error(err)
+            })
+        } else if (file && coverFile) {
+          const uploadFile = file => {
+            return uploadImage(file, 'users', user.id).catch(err => {
+              console.error(err)
+            })
+          }
+
+          const uploadCoverFile = coverFile => {
+            return uploadImage(coverFile, 'users', user.id).catch(err => {
+              console.error(err)
+            })
+          }
+
+          return Promise.all([
+            uploadFile(file),
+            uploadCoverFile(coverFile)
+          ]).then(([profilePhoto, coverPhoto]) => {
+            return (
+              db
+                .table('users')
+                .get(user.id)
+                .update(
+                  {
+                    ...user,
+                    coverPhoto,
+                    profilePhoto
+                  },
+                  { returnChanges: 'always' }
+                )
+                .run()
+                // return the resulting community with the profilePhoto set
+                .then(result => {
+                  // if an update happened
+                  if (result.replaced === 1) {
+                    return result.changes[0].new_val
+                  }
+
+                  // an update was triggered from the client, but no data was changed
+                  if (result.unchanged === 1) {
+                    return result.changes[0].old_val
+                  }
+                })
+            )
+          })
+        }
       } else {
         return db
           .table('users')
@@ -231,14 +377,122 @@ const editUser = (input: EditUserInput, userId: string): Promise<DBUser> => {
     })
 }
 
+type UserThreadCount = {
+  id: string,
+  count: number,
+}
+
+const getUsersThreadCount = (threadIds: Array<string>): Promise<Array<UserThreadCount>> => {
+  const getThreadCounts = threadIds.map(creatorId =>
+    db
+      .table('threads')
+      .getAll(creatorId, { index: 'creatorId' })
+      .count()
+      .run()
+  )
+
+  return Promise.all(getThreadCounts).then(result => {
+    return result.map((threadCount, index) => ({
+      id: threadIds[index],
+      count: threadCount
+    }))
+  })
+}
+
+const setUserOnline = (id: string, isOnline: boolean): DBUser => {
+  let data = {}
+
+  data.isOnline = isOnline
+
+  // If a user is going offline, store their lastSeen
+  if (isOnline === false) {
+    data.lastSeen = new Date()
+  }
+  return db
+    .table('users')
+    .get(id)
+    .update(data, { returnChanges: 'always' })
+    .run()
+    .then(result => {
+      if (result.changes[0].new_val) {
+        const user = result.changes[0].new_val
+        return user
+      }
+      return result.changes[0].old_val
+    })
+}
+
+const setUserPendingEmail = (userId: string, pendingEmail: string): Promise<Object> => {
+  return db
+    .table('users')
+    .get(userId)
+    .update({
+      pendingEmail
+    })
+    .run()
+    .then(async () => {
+      const user = await getUserById(userId)
+      return user
+    })
+}
+
+const updateUserEmail = (userId: string, email: string): Promise<Object> => {
+  return db
+    .table('users')
+    .get(userId)
+    .update({
+      email,
+      pendingEmail: db.literal()
+    })
+    .run()
+    .then(async () => {
+      return getUserById(userId)
+    })
+}
+
+const deleteUser = (userId: string) => {
+  return db
+    .table('users')
+    .get(userId)
+    .update({
+      username: null,
+      email: null,
+      deletedAt: new Date(),
+      providerId: null,
+      fbProviderId: null,
+      googleProviderId: null,
+      githubProviderId: null,
+      githubUsername: null,
+      profilePhoto: null,
+      description: null,
+      website: null,
+      timezone: null,
+      lastSeen: null,
+      modifiedAt: null,
+      firstName: null,
+      lastName: null,
+      pendingEmail: null,
+      name: 'Deleted'
+    })
+    .run()
+}
+
 module.exports = {
-  storeUser,
-  createOrFindUser,
-  saveUserProvider,
   getUser,
-  getUserByIndex,
+  getUserById,
+  getUserByEmail,
   getUserByUsername,
-  getUsers,
   getUsersByUsername,
-  editUser
+  getUsersThreadCount,
+  getUsers,
+  getUserByIndex,
+  saveUserProvider,
+  createOrFindUser,
+  storeUser,
+  editUser,
+  getEverything,
+  setUserOnline,
+  setUserPendingEmail,
+  updateUserEmail,
+  deleteUser
 }
