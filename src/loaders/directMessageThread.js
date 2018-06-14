5ac95e4cd2db200660b0679de09977e03c3862f3
Bran Liang <lby89757@hotmail.com>
2018-06-14 17:03:08 +0800

    Add loader directMessageThread

--- src/loaders/directMessageThread.js
+++ src/loaders/directMessageThread.js
@@ -1,7 +1,9 @@
 // @flow
 import { getDirectMessageThreads } from '../models/directMessageThread'
 import { getMembersInDirectMessageThreads } from '../models/usersDirectMessageThread'
+import { getLastMessages } from '../models/message'
 import createLoader from './createLoader'
+import type { Loader } from '../flowTypes'
 
 export const __createDirectMessageThreadLoader = createLoader(threads => {
   return getDirectMessageThreads(threads)
@@ -10,3 +12,14 @@ export const __createDirectMessageThreadLoader = createLoader(threads => {
 export const __createDirectMessageParticipantsLoader = createLoader(threads => {
   return getMembersInDirectMessageThreads(threads)
 }, 'group')
+
+export const __createDirectMessageSnippetLoader = createLoader(
+  threads => getLastMessages(threads),
+  'group'
+)
+
+export default () => {
+  throw new Error(
+    '⚠️ Do not import loaders directly, get them from the GraphQL context instead! ⚠️'
+  )
+}
