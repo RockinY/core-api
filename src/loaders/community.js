bc137cd52cb0f57276e6d3cad15f4ab017ca188c
Bran Liang <lby89757@hotmail.com>
2018-06-14 16:26:11 +0800

    Add loader community

--- src/loaders/community.js
+++ src/loaders/community.js
@@ -1,8 +1,11 @@
 // @flow
 import {
   getCommunities,
-  getCommunitiesBySlug
+  getCommunitiesBySlug,
+  getCommunitiesMemberCounts,
+  getCommunitiesChannelCounts
 } from '../models/community'
+import { getCommunitiesSettings } from '../models/communitySettings'
 import createLoader from './createLoader'
 
 export const __createCommunityLoader = createLoader(communities => {
@@ -13,3 +16,24 @@ export const __createCommunityBySlugLoader = createLoader(
   communities => getCommunitiesBySlug(communities),
   'slug'
 )
+
+export const __createCommunityMemberCountLoader = createLoader(
+  communityIds => getCommunitiesMemberCounts(communityIds),
+  'group'
+)
+
+export const __createCommunityChannelCountLoader = createLoader(
+  communityIds => getCommunitiesChannelCounts(communityIds),
+  'group'
+)
+
+export const __createCommunitySettingsLoader = createLoader(
+  communityIds => getCommunitiesSettings(communityIds),
+  key => key.communityId
+)
+
+export default () => {
+  throw new Error(
+    '⚠️ Do not import loaders directly, get them from the GraphQL context instead! ⚠️'
+  )
+}
