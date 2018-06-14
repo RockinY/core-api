a9112ea5785b33cb8c729166e16dde6b9148452e
Bran Liang <lby89757@hotmail.com>
2018-06-14 16:23:34 +0800

    Add loader channel

--- src/loaders/channel.js
+++ src/loaders/channel.js
@@ -1,9 +1,39 @@
 // @flow
 import {
-  getChannels
+  getChannels,
+  getChannelsThreadCounts,
+  getChannelsMemberCounts
 } from '../models/channel'
+import { getChannelsSettings } from '../models/channelSettings'
+import { getPendingUsersInChannels } from '../models/usersChannels'
 import createLoader from './createLoader'
 
 export const __createChannelLoader = createLoader(channels => {
   return getChannels(channels)
 })
+
+export const __createChannelThreadCountLoader = createLoader(
+  channels => getChannelsThreadCounts(channels),
+  'group'
+)
+
+export const __createChannelMemberCountLoader = createLoader(
+  channels => getChannelsMemberCounts(channels),
+  'group'
+)
+
+export const __createChannelPendingMembersLoader = createLoader(
+  channels => getPendingUsersInChannels(channels),
+  'group'
+)
+
+export const __createChannelSettingsLoader = createLoader(
+  channelIds => getChannelsSettings(channelIds),
+  key => key.channelId
+)
+
+export default () => {
+  throw new Error(
+    '⚠️ Do not import loaders directly, get them from the GraphQL context instead! ⚠️'
+  )
+}
