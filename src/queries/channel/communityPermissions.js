2b9c38532fbb62267d6c22ca0e71386618119bb6
Bran Liang <lby89757@hotmail.com>
2018-06-14 17:54:53 +0800

    Add query channel community permission

---
+++ src/queries/channel/communityPermissions.js
@@ -0,0 +1,16 @@
+import type { GraphQLContext, DBChannel } from '../../flowTypes'
+
+export default (root: DBChannel, _: any, { user, loaders }: GraphQLContext) => {
+  const communityId = root.id || root.communityId
+  if (!communityId || !user) {
+    return {
+      isOwner: false,
+      isMember: false,
+      isModerator: false,
+      isBlocked: false,
+      isPending: false,
+      receiveNotifications: false
+    }
+  }
+  return loaders.userPermissionsInCommunity.load([user.id, communityId])
+}
