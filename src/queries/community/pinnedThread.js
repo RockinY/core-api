41a4a16bd1063b4d8ff47b9cc1461f81c447e433
Bran Liang <lby89757@hotmail.com>
2018-06-14 21:01:50 +0800

    Add community pinnedThread

---
+++ src/queries/community/pinnedThread.js
@@ -0,0 +1,17 @@
+// @flow
+import type { DBCommunity, GraphQLContext } from '../../flowTypes'
+import { getThreadById } from '../../models/thread'
+import { canViewCommunity } from '../../utils/permissions'
+
+export default async (root: DBCommunity, _: any, ctx: GraphQLContext) => {
+  const { user, loaders } = ctx
+  const { pinnedThreadId, id } = root
+
+  if (!pinnedThreadId) return null
+
+  if (!await canViewCommunity(user, id, loaders)) {
+    return null
+  }
+
+  return await getThreadById(pinnedThreadId)
+}
