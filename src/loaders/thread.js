eed6a10d2b28abd3dbedbea0f25401fb20ebbb50
Bran Liang <lby89757@hotmail.com>
2018-06-14 17:11:40 +0800

    Add loader threads

--- src/loaders/thread.js
+++ src/loaders/thread.js
@@ -1,10 +1,22 @@
 // @flow
 import { getThreads } from '../models/thread'
+import { getMessageCountInThreads } from '../models/message'
+import { getParticipantsInThreads } from '../models/usersThreads'
 import createLoader from './createLoader'
 
 export const __createThreadLoader = createLoader(threads => {
   return getThreads(threads)
 })
+
+export const __createThreadParticipantsLoader = createLoader(
+  threadIds => getParticipantsInThreads(threadIds),
+  'group'
+)
+
+export const __createThreadMessageCountLoader = createLoader(
+  threadIds => getMessageCountInThreads(threadIds),
+  'group'
+)
 
 export default () => {
   throw new Error(
