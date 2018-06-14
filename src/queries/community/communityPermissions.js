51356b07044473568bafe63a7c1ec18c5ea1f42d
Bran Liang <lby89757@hotmail.com>
2018-06-14 20:18:00 +0800

    Add community permission query

---
+++ src/queries/community/communityPermissions.js
@@ -0,0 +1,14 @@
+import { DBCommunity, GraphQLContext } from '../../flowTypes'
+
+export default (
+  { id }: DBCommunity,
+  _: any,
+  { user, loaders }: GraphQLContext
+) => {
+  if (!id || !user) {
+    return {}
+  }
+  return loaders.userPermissionsInCommunity
+    .load([user.id, id])
+    .then(result => (result ? result : {}))
+}
