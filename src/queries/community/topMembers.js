2e6776efab2716d43fc0dfb905024aac327d7c3a
Bran Liang <lby89757@hotmail.com>
2018-06-15 16:48:31 +0800

    Add community topMembers query

---
+++ src/queries/community/topMembers.js
@@ -0,0 +1,40 @@
+// @flow
+import type { DBCommunity, GraphQLContext } from '../../flowTypes'
+import UserError from '../../utils/userError'
+const { getTopMembersInCommunity } = require('../../models/reputationEvents')
+
+export default async (
+  { id }: DBCommunity,
+  _: any,
+  { user, loaders }: GraphQLContext
+) => {
+  const currentUser = user
+
+  if (!currentUser) {
+    return new UserError('You must be signed in to continue.')
+  }
+
+  const { isOwner } = await loaders.userPermissionsInCommunity.load([
+    currentUser.id,
+    id
+  ])
+
+  if (!isOwner) {
+    return new UserError(
+      'You must be the owner of this community to view analytics.'
+    )
+  }
+
+  // $FlowFixMe
+  const userIds = await getTopMembersInCommunity(id)
+
+  if (!userIds || userIds.length === 0) {
+    return []
+  }
+
+  return getTopMembersInCommunity(id).then(usersIds => {
+    const permissionsArray = usersIds.map(userId => [userId, id])
+    // $FlowIssue
+    return loaders.userPermissionsInCommunity.loadMany(permissionsArray)
+  })
+}
