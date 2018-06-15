883a4e792c22fee8e5159edf0d95e57dcfc53406
Bran Liang <lby89757@hotmail.com>
2018-06-15 19:49:46 +0800

    Add direct message thread

--- src/queries/community/index.js
+++ src/queries/community/index.js
@@ -1,23 +1,23 @@
 // @flow
-import community from './rootCommunity';
-import communities from './rootCommunities';
-import topCommunities from './rootTopCommunities';
-import recentCommunities from './rootRecentCommunities';
+import community from './rootCommunity'
+import communities from './rootCommunities'
+import topCommunities from './rootTopCommunities'
+import recentCommunities from './rootRecentCommunities'
 
-import communityPermissions from './communityPermissions';
-import channelConnection from './channelConnection';
-import members from './members';
-import pinnedThread from './pinnedThread';
-import threadConnection from './threadConnection';
-import metaData from './metaData';
-import memberGrowth from './memberGrowth';
-import conversationGrowth from './conversationGrowth';
-import topMembers from './topMembers';
-import topAndNewThreads from './topAndNewThreads';
-import contextPermissions from './contextPermissions';
-import watercooler from './watercooler';
-import brandedLogin from './brandedLogin';
-import joinSettings from './joinSettings';
+import communityPermissions from './communityPermissions'
+import channelConnection from './channelConnection'
+import members from './members'
+import pinnedThread from './pinnedThread'
+import threadConnection from './threadConnection'
+import metaData from './metaData'
+import memberGrowth from './memberGrowth'
+import conversationGrowth from './conversationGrowth'
+import topMembers from './topMembers'
+import topAndNewThreads from './topAndNewThreads'
+import contextPermissions from './contextPermissions'
+import watercooler from './watercooler'
+import brandedLogin from './brandedLogin'
+import joinSettings from './joinSettings'
 
 module.exports = {
   Query: {
@@ -41,6 +41,5 @@ module.exports = {
     watercooler,
     brandedLogin,
     joinSettings
-  },
-};
-
+  }
+}
