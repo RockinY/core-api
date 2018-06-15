9e3618ffa3713d927e172bbc00f04b6ac99506aa
Bran Liang <lby89757@hotmail.com>
2018-06-15 19:28:48 +0800

    Add type direct message thread

--- src/types/DirectMessageThread.js
+++ src/types/DirectMessageThread.js
@@ -1,10 +1,34 @@
 // @flow
 const DirectMessageThread = `
-  type DirectMessageThread {
-    id: ID!
-    threadLastActive: Date!
+  type DirectMessagesConnection {
+    pageInfo: PageInfo!
+    edges: [DirectMessageEdge!]
   }
 
+  type DirectMessageEdge {
+    cursor: String!
+    node: Message!
+  }
+
+  type ParticipantInfo {
+    id: ID!
+    name: String
+    username: String
+    profilePhoto: String
+    lastActive: Date
+    lastSeen: Date
+    userId: ID!
+    isOnline: Boolean
+  }
+
+  type DirectMessageThread {
+    id: ID!
+    messageConnection(first: Int = 20, after: String): DirectMessagesConnection! @cost(complexity: 1, multiplier: "first")
+    participants: [ParticipantInfo]! @cost(complexity: 1)
+    snippet: String! @cost(complexity: 2)
+    threadLastActive: Date!
+  }
+  
   extend type Query {
     directMessageThread(id: ID!): DirectMessageThread
   }
