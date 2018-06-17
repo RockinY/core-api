6b499c753b3ebe96bed2f3d3748d1689ed74e4e9
Bran Liang <lby89757@hotmail.com>
2018-06-17 15:05:55 +0800

    Add update user email mutation

--- src/mutations/user/updateUserEmail.js
+++ src/mutations/user/updateUserEmail.js
@@ -1,40 +1,38 @@
 // @flow
-import type { GraphQLContext } from '../../';
-import UserError from '../../utils/UserError';
-import { getUserByEmail, setUserPendingEmail } from '../../models/user';
-import isEmail from 'validator/lib/isEmail';
-import { sendEmailValidationEmailQueue } from 'shared/bull/queues';
-import { isAuthedResolver as requireAuth } from '../../utils/permissions';
+import type { GraphQLContext } from '../../flowTypes'
+import UserError from '../../utils/userError'
+import { getUserByEmail, setUserPendingEmail } from '../../models/user'
+import isEmail from 'validator/lib/isEmail'
+import { isAuthedResolver as requireAuth } from '../../utils/permissions'
 
 type Input = {
   email: string,
 };
 
 export default requireAuth(async (_: any, args: Input, ctx: GraphQLContext) => {
-  const { email } = args;
-  const { user } = ctx;
+  const { email } = args
+  const { user } = ctx
 
   if (!isEmail(email)) {
-    return new UserError('Please enter a working email address');
+    return new UserError('Please enter a working email address')
   }
 
-  const result = await getUserByEmail(email);
+  const result = await getUserByEmail(email)
 
   if (result && result.email === email) {
     return new UserError(
       'Another person on Spectrum is already using this email.'
-    );
+    )
   }
 
   return await setUserPendingEmail(user.id, email)
     .then(user => {
-      sendEmailValidationEmailQueue.add({ email, userId: user.id });
-      return user;
+      return user
     })
     .catch(
       err =>
         new UserError(
           "We weren't able to send a confirmation email. Please try again."
         )
-    );
-});
+    )
+})
