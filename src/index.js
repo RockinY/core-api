9bdf738aa25d966928991bd355f3b7b6d49dcfc3
Bran Liang <lby89757@hotmail.com>
2018-07-19 20:06:38 +0800

    Add fileupload middleware

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { createServer } from 'http'
 import express from 'express'
 import { ApolloEngine } from 'apollo-engine'
 import toobusy from './middlewares/toobusy'
+import { apolloUploadExpress } from 'apollo-upload-server'
 import addSecurityMiddleware from './middlewares/security'
 import logging from './middlewares/logging'
 import jwtAuth from './middlewares/auth'
@@ -32,35 +33,39 @@ const app = express()
 app.set('trust proxy', true)
 
 /* ----------- Middlewares ----------- */
-// 1. Send all responses as gzip
+// *. Send all responses as gzip
 app.use(compression())
-// 2. Toobusy situation
+// *. Toobusy situation
 app.use(toobusy)
-// 3. Increase security
+// *. Increase security
 addSecurityMiddleware(app)
-// 4. Logging
+// *. Logging
 if (process.env.NODE_ENV === 'development') {
   app.use(logging)
 }
-// 5. JWT header cookie
+// *. JWT header cookie
 app.use(jwtAuth)
-// 6. Cross Origin Request
+// *. Cross Origin Request
 app.use(cors({
   origin: ['http://localhost:5000', 'https://www.liangboyuan.pub'],
   credentials: true
 }))
-// 7. Cookie parser
+// *. Cookie parser
 app.use(cookieParser())
-// 8. JSON body parser
+// *. Enable file upload
+app.use(apolloUploadExpress({
+  maxFileSize: 25 * 1024 * 1024
+}))
+// *. JSON body parser
 app.use(bodyParser.json())
-// 9. Session
+// *. Session
 app.use(session)
-// 10. passport
+// *. passport
 app.use(passport.initialize())
 app.use(passport.session())
-// 11. redirect
+// *. redirect
 app.use(threadParamRedirect)
-// 12. Catch Error
+// *. Catch Error
 app.use(
   (
     err: Error,
