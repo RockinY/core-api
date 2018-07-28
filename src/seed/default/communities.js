f45441b1e4175cf9aac7bdbe2da340af9f817da8
Bran Liang <lby89757@hotmail.com>
2018-07-28 20:10:00 +0800

    Add sample private community

--- src/seed/default/communities.js
+++ src/seed/default/communities.js
@@ -4,13 +4,15 @@ const {
   DATE,
   SPECTRUM_COMMUNITY_ID,
   PAYMENTS_COMMUNITY_ID,
-  DELETED_COMMUNITY_ID
+  DELETED_COMMUNITY_ID,
+  PRIVATE_COMMUNITY_ID
 } = constants
 
 module.exports = [
   {
     id: SPECTRUM_COMMUNITY_ID,
     createdAt: new Date(DATE),
+    isPrivate: false,
     name: '云社官方',
     description: '一个自由和谐有趣的社区',
     website: 'https://yunshe.fun',
@@ -23,6 +25,7 @@ module.exports = [
   {
     id: PAYMENTS_COMMUNITY_ID,
     createdAt: new Date(DATE),
+    isPrivate: false,
     name: '南京酷猿信息技术有限公司',
     description: '全蓝鲸最厉害的互联网公司',
     website: 'https://corran.cn',
@@ -36,6 +39,7 @@ module.exports = [
     id: DELETED_COMMUNITY_ID,
     createdAt: new Date(DATE),
     deletedAt: new Date(DATE),
+    isPrivate: false,
     name: '已被删除',
     description: '不会再出现的',
     website: 'https://yunshe.fun',
@@ -44,5 +48,18 @@ module.exports = [
     coverPhoto:
       'https://xlabsample.oss-cn-hangzhou.aliyuncs.com/covers/cover2.jpg',
     slug: 'deleted'
-  }
+  },
+  {
+    id: PRIVATE_COMMUNITY_ID,
+    createdAt: new Date(DATE),
+    isPrivate: true,
+    name: '秘密社区',
+    description: '请注意，这是私人社区',
+    website: 'https://yunshe.fun',
+    profilePhoto:
+      'https://xlabsample.oss-cn-hangzhou.aliyuncs.com/communities/comm3.png',
+    coverPhoto:
+    'https://xlabsample.oss-cn-hangzhou.aliyuncs.com/covers/cover20.jpg',
+    slug: 'private',
+  },
 ]
