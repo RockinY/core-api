0ea0d8d1e5dfe011a7eadb4e13f13cd97cbcafa8
Bran Liang <lby89757@hotmail.com>
2018-07-28 18:47:01 +0800

    Enable playground whatever

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -5,10 +5,10 @@ import graphql from './graphql'
 
 const apiRouter = Router()
 
-if (process.env.NODE_ENV !== 'production') {
-  apiRouter.use('/playground', graphiql)
-}
-
+// if (process.env.NODE_ENV !== 'production') {
+//   apiRouter.use('/playground', graphiql)
+// }
+apiRouter.use('/playground', graphiql)
 apiRouter.use('/', graphql)
 
 export default apiRouter
