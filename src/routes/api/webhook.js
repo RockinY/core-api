c69a44f3b3d6a4d60c15a9502a0a67ecc4f8ff67
Bran Liang <lby89757@hotmail.com>
2018-08-18 21:29:01 +0800

    Update the alipay failure check

--- src/routes/api/webhook.js
+++ src/routes/api/webhook.js
@@ -19,32 +19,32 @@ webhookRouter.post('/alipay', async (req, res) => {
   
   if (!alipaySdk.checkNotifySign(body)) {
     debug('Alipay aign failed.')
-    return res.status(400).send('FAILED');
+    return res.status(400).send('failure');
   }
 
   // 商户需要验证该通知数据中的out_trade_no是否为商户系统中创建的订单号
   const invoice = await getInvoiceByTradeNo(body.out_trade_no)
   if (!invoice || invoice.paid) {
     debug('Invalid invoice.')
-    return res.status(400).send('FAILED');
+    return res.status(400).send('failure');
   }
 
   // 判断total_amount是否确实为该订单的实际金额（即商户订单创建时的金额）
   if (parseFloat(invoice.amount / 100).toFixed(2) !== body.total_amount) {
     debug('Invalid invoice amount.')
-    return res.status(400).send('FAILED');
+    return res.status(400).send('failure');
   }
 
   // 校验通知中的seller_id（或者seller_email) 是否为out_trade_no这笔单据的对应的操作方
   if (body.seller_id !== process.env.ALIPAY_SELLER_ID) {
     debug('Invalid seller id.')
-    return res.status(400).send('FAILED');
+    return res.status(400).send('failure');
   }
 
   // 验证app_id是否为该商户本身
   if (body.app_id !== process.env.ALIPAY_OAUTH_CLIENT_ID) {
     debug('Invalid app_id.')
-    return res.status(400).send('FAILED');
+    return res.status(400).send('failure');
   }
 
   if (body.trade_status === 'TRADE_SUCCESS') {
@@ -54,7 +54,7 @@ webhookRouter.post('/alipay', async (req, res) => {
         return res.status(200).send('success');
       })
   } else {
-    return res.status(400).send('FAILED');
+    return res.status(400).send('failure');
   }
 })
 
