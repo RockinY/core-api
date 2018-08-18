d19cdb473fb48e94a8314a0651239b5ddcc6e0b0
Bran Liang <lby89757@hotmail.com>
2018-08-18 12:50:29 +0800

    Update webhook query

--- src/routes/api/webhook.js
+++ src/routes/api/webhook.js
@@ -11,44 +11,49 @@ const webhookRouter = Router()
 // $FlowFixMe
 webhookRouter.post('/alipay', async (req, res) => {
   // 异步通知验签
-  debug(JSON.stringify(req.body))
+  debug(JSON.stringify(req))
+  debug(JSON.stringify(req.query))
+
+  const body = req.query
   
-  if (!alipaySdk.checkNotifySign(req.body)) {
+  if (!alipaySdk.checkNotifySign(body)) {
     debug('Alipay aign failed.')
-    return
+    return res.status(400).send('FAILED');
   }
 
   // 商户需要验证该通知数据中的out_trade_no是否为商户系统中创建的订单号
-  const invoice = await getInvoiceByTradeNo(req.body.out_trade_no)
+  const invoice = await getInvoiceByTradeNo(body.out_trade_no)
   if (!invoice || invoice.paid) {
     debug('Invalid invoice.')
-    return
+    return res.status(400).send('FAILED');
   }
 
   // 判断total_amount是否确实为该订单的实际金额（即商户订单创建时的金额）
-  if (parseFloat(invoice.amount / 100).toFixed(2) !== req.body.total_fee) {
+  if (parseFloat(invoice.amount / 100).toFixed(2) !== body.total_fee) {
     debug('Invalid invoice amount.')
-    return
+    return res.status(400).send('FAILED');
   }
 
   // 校验通知中的seller_id（或者seller_email) 是否为out_trade_no这笔单据的对应的操作方
-  if (req.body.seller_id !== process.env.ALIPAY_SELLER_ID) {
+  if (body.seller_id !== process.env.ALIPAY_SELLER_ID) {
     debug('Invalid seller id.')
-    return
+    return res.status(400).send('FAILED');
   }
 
   // 验证app_id是否为该商户本身
-  if (req.body.app_id !== process.env.ALIPAY_OAUTH_CLIENT_ID) {
+  if (body.app_id !== process.env.ALIPAY_OAUTH_CLIENT_ID) {
     debug('Invalid app_id.')
-    return
+    return res.status(400).send('FAILED');
   }
 
-  if (req.body.trade_status === 'TRADE_SUCCESS') {
+  if (body.trade_status === 'TRADE_SUCCESS') {
     debug('Sign success.')
     createMemberSubscription(invoice)
       .then(() => {
-        res.status(200).send('SUCCESS');
+        return res.status(200).send('SUCCESS');
       })
+  } else {
+    return res.status(400).send('FAILED');
   }
 })
 
