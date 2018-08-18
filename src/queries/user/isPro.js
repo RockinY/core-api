85ae81aaae81fe344ec1262fc6a7a98e85ffa2be
Bran Liang <lby89757@hotmail.com>
2018-08-18 22:21:44 +0800

    Enable user pro check

--- src/queries/user/isPro.js
+++ src/queries/user/isPro.js
@@ -1,4 +1,14 @@
-// All user is pro for the moment
-export default () => {
-  return false
+// @flow
+import dayjs from 'dayjs'
+import type { GraphQLContext, DBUser } from '../../flowTypes'
+import { getMemberSubscriptionsByuserId } from '../../models/memberSubscription'
+
+export default async ({ id }: DBUser) => {
+  const userSubscriptions = await getMemberSubscriptionsByuserId(id)
+  const latestSubscription = userSubscriptions[0]
+  if (!latestSubscription || dayjs(latestSubscription.endAt).isBefore(dayjs())) {
+    return false
+  }
+
+  return true
 }
