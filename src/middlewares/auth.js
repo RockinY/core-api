da9ac7224bd62de854337289bbfb4c77af0e085f
Bran Liang <lby89757@hotmail.com>
2018-08-21 19:48:22 +0800

    Add debug on auth

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,10 +1,14 @@
 import jwt from 'jsonwebtoken'
 
+const debug = require('debug')('api')
+
 export default (req, res, next) => {
   if (req.headers && !req.headers.cookie && req.headers.authorization) {
     const token = req.headers.authorization.replace(/^\s*Bearer\s*/, '')
+    debug(token)
     try {
       const decoded = jwt.verify(token, process.env.API_TOKEN_SECRET)
+      debug(decoded)
       if (decoded.cookie) req.headers.cookie = decoded.cookie
     } catch (err) {}
   }
